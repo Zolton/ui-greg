a312681128b27159be6b166d584efe8a7b4c1831
Zolton <gmzesq@gmail.com>
2019-05-22 14:14:28 -0400

    JS working

--- js/Macro Calculator JS.js
+++ js/Macro Calculator JS.js
@@ -4,13 +4,22 @@
 //     interval: 1000
 //   })
 
-const homeLink = document.querySelector(".home-link");
-const aboutUs = document.querySelector(".about-us-link");
-const loginButton = document.querySelector("button");
+const homeLink = document.querySelectorAll(".home-link");
+const aboutUs = document.querySelectorAll(".about-us-link");
+const loginButton = document.querySelectorAll(".button1");
+const languageButton = document.querySelector(".button2")
 
-homeLink.textContent = "家";
-homeLink.style.fontSize = "2rem";
-aboutUs.textContent = "私達";
-aboutUs.style.fontSize = "2rem";
-loginButton.textContent = "証明の画面";
-loginButton.style.fontSize = "2rem";
+languageButton.addEventListener("click", activateVoltron)
+
+function activateVoltron () {
+homeLink.forEach(change => change.textContent = "家");
+homeLink.forEach(change => change.style.fontSize = "2rem");
+aboutUs.forEach(change => change.textContent = "私達");
+aboutUs.forEach(change => change.style.fontSize = "2rem");
+loginButton.forEach(change => change.textContent = "証明の画面");
+loginButton.forEach(change => change.style.fontSize = "2rem");
+languageButton.style.display = "none";
+}
+
+
+
