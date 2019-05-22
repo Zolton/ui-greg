96b80a59c891bff4285da016ecda2e99227f130d
Zolton <gmzesq@gmail.com>
2019-05-22 13:29:01 -0400

    WIP JS MVP features

--- js/Macro Calculator JS.js
+++ js/Macro Calculator JS.js
@@ -1,5 +1,16 @@
 // $('.carousel').carousel()
 
-$('.carousel').carousel({
-    interval: 1000
-  })
+// $('.carousel').carousel({
+//     interval: 1000
+//   })
+
+const homeLink = document.querySelector(".home-link");
+const aboutUs = document.querySelector(".about-us-link");
+const loginButton = document.querySelector("button");
+
+homeLink.textContent = "家";
+homeLink.style.fontSize = "2rem";
+aboutUs.textContent = "私達";
+aboutUs.style.fontSize = "2rem";
+loginButton.textContent = "証明の画面";
+loginButton.style.fontSize = "2rem";
